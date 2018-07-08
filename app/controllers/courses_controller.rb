38510116bc5ef66a75c143e15c8a0afadf996fb8
BrokenPirate <i.am.ugogessa@gmail.com>
2018-07-08 20:30:08 +0800

    Add Post Course Reques form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,29 +6,29 @@ class CoursesController < ApplicationController
 	def index
 		@courses = Course.all
 	end
-
 	def show
-		
 		@course = Course.find(params[:id])
 	end
-
 	def new
-
+		@course = Course.new
 	end
-
 	def create
+		@course = Course.new(course_params)
+		@course.save
+		redirect_to courses_path
 
 	end
-
 	def edit
 
 	end
-
 	def update
 
 	end
-
 	def destroy
 
+	end
+
+	def course_params
+		params.require(:course).permit(:name, :url, :tagline)
 	end
 end
